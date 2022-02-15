2c14b22c43cda8ff22983ed03500e4dcff1168f1
hwikle <henry.wikle@gmail.com>
2022-02-15 12:39:08 -0700

    Stub out Tile and Constraint

---
+++ src/Tile.java
@@ -0,0 +1,12 @@
+public class Tile {
+    private char letter;
+    private int value;
+
+    public char getLetter() {
+        return this.letter;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+}
